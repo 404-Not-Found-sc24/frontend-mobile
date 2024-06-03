84257a0fb8dc942d1d26bd63349096bebe0b1a9e
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-06-04 03:54:30 +0900

    feat: myplanpage 모바일 css 수정

--- src/components/MyPlanDetailBox.tsx
+++ src/components/MyPlanDetailBox.tsx
@@ -39,24 +39,30 @@ const MyPlanDetailBox: React.FC<PlanDetailBoxProps> = ({
       className="w-full h-[15%] p-5 flex rounded-md shadow-xl mb-5"
       onClick={toDiaryDetail}
     >
-      <div className="w-full flex items-center">
-        <div className="w-[10%] font-['BMJUA'] text-[#FF9A9A] text-xl">
-          {time}
+      <div className="w-full flex justify-between items-center">
+        <div className="flex flex-row w-[75%]">
+          <div className="font-['BMJUA'] text-[#FF9A9A] text-xl mr-3">
+            {time}
+          </div>
+          <div className="flex flex-col">
+            <div className="font-['BMJUA'] text-lg">{locationName.length > 7 ? `${locationName.slice(0, 6)}...` : locationName}</div>
+            <div className="font-['Nanum Gothic'] text-md pr-2">{content}</div>
+          </div>
         </div>
-        <div className="w-[22%] font-['BMJUA'] text-2xl">{locationName.length > 7 ? `${locationName.slice(0, 6)}...` : locationName}</div>
-        <div className="w-[43%] font-['Nanum Gothic'] pr-2">{content}</div>
         <div className="w-[25%]">
           {imageUrl ? (
-              <img
-                  src={imageUrl}
-                  width="250px"
-                  alt="일기사진"
-              />
+            <img
+              src={imageUrl}
+              className="w-full object-cover"
+              alt="일기사진"
+              style={{ maxWidth: '100%' }}
+            />
           ) : (
-              <div
-                  className="w-[100%] h-[60px] border-2 text-center flex items-center justify-center">
+            <div
+              className="w-full border-2 text-center flex items-center justify-center"
+              style={{ maxWidth: '100%' }}>
               No Image
-              </div>
+            </div>
           )}
 
         </div>
