4cf891bb7cb8e2f1f79e732eb4807632ca0b4e1c
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-06-04 02:22:22 +0900

    fix:navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
 const NavBar: React.FC = () => {
   const { isAuthenticated, logout } = useAuth();
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleLogout = () => {
     logout();
+  };
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
   };
 
   const navigateToTravel = (curr: string) => {
@@ -19,7 +24,7 @@ const NavBar: React.FC = () => {
   };
 
   return (
-    <nav className="bg-white border-gray-200 h-[10%]">
+    <nav className="h-[15%] bg-white border-gray-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link
           to="/"
@@ -29,10 +34,34 @@ const NavBar: React.FC = () => {
             나들이
           </span>
         </Link>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <div>
+        <button
+          onClick={toggleMenu}
+          type="button"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+          aria-controls="navbar-default"
+          aria-expanded={isOpen}
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5h14a1 1 0 010 2H3a1 1 0 010-2zM3 10h14a1 1 0 010 2H3a1 1 0 010-2zM3 15h14a1 1 0 010 2H3a1 1 0 010-2z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        <div
+          className={`${isOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`}
+          id="navbar-default"
+        >
+          <ul className="font-5xl flex flex-col md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white items-center">
             {isAuthenticated ? (
-              <ul className="font-5xl flex p-4 md:p-0 mt-4 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white items-center">
+              <>
                 <li>
                   <Link
                     to="/event"
@@ -43,9 +72,9 @@ const NavBar: React.FC = () => {
                 </li>
                 <li>
                   <Link
-                      to="/traveldes"
-                      state= {{ curr: 'tour'}}
-                      className="py-4 px-3 item-center text-main-green-color font-BMJUA"
+                    to="/traveldes"
+                    state={{ curr: 'tour' }}
+                    className="py-4 px-3 item-center text-main-green-color font-BMJUA"
                   >
                     여행지
                   </Link>
@@ -67,9 +96,9 @@ const NavBar: React.FC = () => {
                     로그아웃
                   </Link>
                 </li>
-              </ul>
+              </>
             ) : (
-              <ul className="font-2xl flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white items-center">
+              <>
                 <li>
                   <Link
                     to="/signup"
@@ -78,7 +107,7 @@ const NavBar: React.FC = () => {
                     회원가입
                   </Link>
                 </li>
-                <li className="flex-shrink-0" >
+                <li className="flex-shrink-0">
                   <Link
                     to="/signin"
                     className="py-2 px-3 border-2 border-white hover:border-main-green-color text-main-green-color font-BMJUA"
@@ -86,9 +115,9 @@ const NavBar: React.FC = () => {
                     로그인
                   </Link>
                 </li>
-              </ul>
+              </>
             )}
-          </div>
+          </ul>
         </div>
       </div>
     </nav>
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,31 +1,24 @@
 import React from 'react';
-import SearchBar from '../components/SearchBar';
+
 const Main: React.FC = () => {
-    return (
-        <div>
-            <div className="w-full flex justify-center mt-10">
-                <div className="w-2/3 sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2">
-                    <SearchBar curr={'main'}/>
-                </div>
-            </div>
-            <div className="flex justify-center mt-[5%]">
-                <div className="container flex flex-row justify-between w-[80%] mt-12">
-                    <div
-                        className="flex flex-col justify-center w-1/2 text-left text-2xl sm:text-4xl md:text-4xl lg:text-6xl xl:text-6xl font-['BMHANNAPro']">
-                        <p className="text-main-red-color ">국내 여행을</p>
-                        <p>한손에서 간편하게</p>
-                    </div>
-                    <div className="w-1/2 h-auto px-4 overflow-hidden shadow-xl md:my-4 lg:my-12 xl:my-4 rounded-2xl">
-                        <div className="relative flex item-center justify-center w-full h-full">
-                            <img
-                                src="/image/나들이.png"
-                                alt="메인페이지 이미지"
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="w-full h-[85%] flex justify-center items-center">
+      <div className="container w-[100%] h-[80%] flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center w-1/2 text-left text-2xl sm:text-4xl md:text-4xl lg:text-6xl xl:text-6xl font-['BMHANNAPro']">
+          <p className="text-main-red-color ">국내 여행을</p>
+          <p>한손에서 간편하게</p>
         </div>
-    );
+        <div className="w-[80%] h-[80%]  px-4 overflow-hidden shadow-xl md:my-4 lg:my-12 xl:my-4 rounded-2xl">
+          <div className="flex item-center justify-center w-full h-full">
+            <img
+              src="/image/나들이.png"
+              alt="메인페이지 이미지"
+              className=" w-[100%] h-[100%] object-cover"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Main;
