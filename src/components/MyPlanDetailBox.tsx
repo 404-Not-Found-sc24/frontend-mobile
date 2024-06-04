3cef69c252802bd825fd97a1eb842199f8bc3bb3
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-06-04 15:40:05 +0900

    fix: 일정 조회 페이지 ui 수정

--- src/components/MyPlanDetailBox.tsx
+++ src/components/MyPlanDetailBox.tsx
@@ -32,38 +32,45 @@ const MyPlanDetailBox: React.FC<PlanDetailBoxProps> = ({
     });
   };
 
-  const { time, locationName, content, imageUrl } = scheduleData;
+  const { time, locationName, title, content, imageUrl } = scheduleData;
 
   return (
     <div
       className="w-full h-[15%] p-5 flex rounded-md shadow-xl mb-5"
-      onClick={toDiaryDetail}
     >
-      <div className="w-full flex justify-between items-center">
-        <div className="flex flex-row w-[75%]">
-          <div className="font-['BMJUA'] text-[#FF9A9A] text-xl mr-3">
-            {time}
-          </div>
-          <div className="flex flex-col">
-            <div className="font-['BMJUA'] text-lg">{locationName.length > 7 ? `${locationName.slice(0, 6)}...` : locationName}</div>
-            <div className="font-['Nanum Gothic'] text-md pr-2">{content}</div>
+      <div className="w-full flex items-center justify-between">
+        <div className="flex flex-row items-center">
+          <img src="icon-pencil.png" alt="일기 수정"
+            className="h-5 w-5 cursor-pointer mr-2"
+            onClick={toDiaryDetail} />
+          <div className="flex flex-col w-[70%]">
+            <div className="w-fit flex flex-col justify-center items-start font-['BMJUA'] text-[#FF9A9A] text-xl">
+              {time}
+              <span className="font-['BMJUA'] text-black text-md">{locationName.length > 6 ? `${locationName.slice(0, 5)}...` : locationName}</span>
+            </div>
+            <div className="w-full flex flex-1 font-['Nanum Gothic'] pr-2">{content}</div>
           </div>
         </div>
         <div className="w-[25%]">
           {imageUrl ? (
+            <div className="w-full h-[60px]">
             <img
               src={imageUrl}
-              className="w-full object-cover"
+              className='w-full h-full object-cover'
               alt="일기사진"
-              style={{ maxWidth: '100%' }}
             />
+          </div>
           ) : (
-              <img src={process.env.PUBLIC_URL + '/image/logo.png'} className='h-full w-full'></img>
-          )}
-
+            <div className="w-[100%] h-[60px] flex items-center justify-center">
+              <div className="text-center text-main-green-color font-bold font-BMJUA">
+                {title || content ? '' : '일기가 없습니다.'}
+              </div>
+            </div>
+          )
+          }
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
