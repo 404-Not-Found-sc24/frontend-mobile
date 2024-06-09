44ff0b38088cc74d78e7d6435826bef54e342ea5
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-06-09 23:16:22 +0900

    fix: 지도 오류 수정

--- src/components/SmallMap.tsx
+++ src/components/SmallMap.tsx
@@ -17,9 +17,9 @@ const SmallMap: React.FC<MapProps> = ({ latitude, longitude }) => {
 
     useEffect(() => {
         console.log("api: ", process.env.REACT_APP_KAKAO_MAP_API_KEY)
+        if (window.kakao && window.kakao.maps) {
+            const container = document.getElementById('map');
 
-        const container = document.getElementById('map');
-        if (container) {
             const options = {
                 center: new window.kakao.maps.LatLng(latitude, longitude),
                 level: 3
